ce7e9f4802772d424871447983032efdde8f6f1b
Seyyed Mahdi Mousavi <sarbaz14zaman@gmail.com>
2024-07-05 20:50:48 +0330

    Update file User

--- RestaurantManagementSystem/Models/User.cs
+++ RestaurantManagementSystem/Models/User.cs
@@ -1,12 +1,28 @@
-﻿namespace RestaurantManagementSystem.Models
+﻿using System.Collections.Generic;
+
+namespace RestaurantManagementSystem.Models
 {
     public class User
     {
-        public string FullName { get; set; }
+        public static List<Customer> customers { get; set; }
+        public static List<RestaurantManager> restaurantManagers { get; set; }
+        public static List<Admin> admins {  get; set; } 
+        public int Id { get; set; }
         public string Username { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string Gender { get; set; }
-        public string HomeAddress { get; set; }
+        public string Password { get; set; }
+
+        public User(int id, string username, string password)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+        }
+
+        static User()
+        {
+            customers = new List<Customer>();
+            restaurantManagers = new List<RestaurantManager>();
+            admins = new List<Admin>();
+        }
     }
 }
