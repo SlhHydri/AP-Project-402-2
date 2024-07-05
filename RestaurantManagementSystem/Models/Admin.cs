111b76e8fd774528daad0972adb31c410ff9ba63
Seyyed Mahdi Mousavi <sarbaz14zaman@gmail.com>
2024-07-05 20:57:33 +0330

    Creating file Admin

---
+++ RestaurantManagementSystem/Models/Admin.cs
@@ -0,0 +1,17 @@
+﻿// In the name of Allah
+
+namespace RestaurantManagementSystem.Models
+{
+    public class Admin : User
+    {
+        public Admin(string Username, string Password) : base ( (User.admins.Count + 1) * 1000,Username, Password)
+        {
+
+        }
+
+        public Admin(int Id, string Username, string Password) : base(Id, Username, Password)
+        {
+
+        }
+    }
+}
