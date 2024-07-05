a54f21c91ed6525c2f0d719a39e8b350bdcce63b
Seyyed Mahdi Mousavi <sarbaz14zaman@gmail.com>
2024-07-05 21:05:23 +0330

    Creating file RestaurantManager

---
+++ RestaurantManagementSystem/Models/RestaurantManager.cs
@@ -0,0 +1,36 @@
+﻿// In the name oa Allah
+
+using System.Linq;
+
+namespace RestaurantManagementSystem.Models
+{
+    public class RestaurantManager : User
+    {
+        public string NameOfRestaurant { get; set; }
+        public string Address { get; set; }
+        public string City {  get; set; }
+        public enum EType { Delivery, Dine_in }
+        public EType Type { get; set; }
+        public double Score { get; set; }
+        
+        public RestaurantManager(string Username, string Password, string nameOfRestaurant, string address, string city, EType type, double score) : base((User.restaurantManagers.Count() + 1) * 1000, Username, Password)
+        {
+            NameOfRestaurant = nameOfRestaurant;
+            Address = address;
+            City = city;
+            Type = type;
+            Score = score;
+        }
+
+        public RestaurantManager(int Id, string Username, string Password, string nameOfRestaurant, string address, string city, EType type, double score) : base(Id, Username, Password)
+        {
+            NameOfRestaurant = nameOfRestaurant;
+            Address = address;
+            City = city;
+            Type = type;
+            Score = score;
+        }
+
+
+    }
+}
