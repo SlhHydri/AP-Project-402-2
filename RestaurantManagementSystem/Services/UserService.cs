98b84d903281a5412deadb4fe607b62710019a3b
Seyyed Mahdi Mousavi <sarbaz14zaman@gmail.com>
2024-07-06 17:07:02 +0330

    fix bugs

--- RestaurantManagementSystem/Services/UserService.cs
+++ RestaurantManagementSystem/Services/UserService.cs
@@ -5,18 +5,18 @@ namespace RestaurantManagementSystem.Services
     public class UserService
     {
         // Dummy implementation for the example
-        public User GetLoggedInUser()
-        {
-            return new User
-            {
-                FullName = "John Doe",
-                Username = "john.doe",
-                PhoneNumber = "123456789",
-                Email = "john.doe@example.com",
-                Gender = "Male",
-                HomeAddress = "123 Main St"
-            };
-        }
+        //public User GetLoggedInUser()
+        //{
+        //    return new User
+        //    {
+        //        FullName = "John Doe",
+        //        Username = "john.doe",
+        //        PhoneNumber = "123456789",
+        //        Email = "john.doe@example.com",
+        //        Gender = "Male",
+        //        HomeAddress = "123 Main St"
+        //    };
+        //}
 
         public void UpdateUserProfile(User user)
         {
